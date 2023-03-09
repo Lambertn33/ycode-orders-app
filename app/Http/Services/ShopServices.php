57ed54450c619b95b84bc40a952a1bd963b0e82d
Lambertn33 <lambertgithub@gmail.com>
2023-03-09 09:57:49 +0200

    chore: align codes

--- app/Http/Services/ShopServices.php
+++ app/Http/Services/ShopServices.php
@@ -48,37 +48,35 @@
     {
       return Cart::where('user_id', $userId)->delete();
     }
-
-
-       // since the ID is autoincrement ,retreive the last saved order to get its ID and use it as in creating slugs
-       public function getLastSavedOrder()
-       {
-         $ordersCollectionName = "Orders";
-         $collectionId = (new CollectionsServices)->getCollectionId($ordersCollectionName);
-         //include query params in url to get last saved order
-         $url = $this->endpointUrl.'/'.$collectionId. '/items?sort_by=ID&order_by=desc&limit=1';
-         return (new CollectionsServices)->getCollectionItems($url);
-       }
+    // since the ID is autoincrement ,retreive the last saved order to get its ID and use it as in creating slugs
+    public function getLastSavedOrder()
+    {
+      $ordersCollectionName = "Orders";
+      $collectionId = (new CollectionsServices)->getCollectionId($ordersCollectionName);
+      //include query params in url to get last saved order
+      $url = $this->endpointUrl.'/'.$collectionId. '/items?sort_by=ID&order_by=desc&limit=1';
+      return (new CollectionsServices)->getCollectionItems($url);
+    }
    
-       public function saveNewOrder($newOrder)
-       {
-         $ordersCollectionName = "Orders";
-         $collectionId = (new CollectionsServices)->getCollectionId($ordersCollectionName);
-         $url = $this->endpointUrl.'/'.$collectionId. '/items';
-         return Http::withHeaders($this->headers)->post($url, $newOrder);
-       }
+    public function saveNewOrder($newOrder)
+    {
+      $ordersCollectionName = "Orders";
+      $collectionId = (new CollectionsServices)->getCollectionId($ordersCollectionName);
+      $url = $this->endpointUrl.'/'.$collectionId. '/items';
+      return Http::withHeaders($this->headers)->post($url, $newOrder);
+    }
    
-       public function saveNewOrderItem($newOrderItems)
-       {
-          $orderItemsCollectionName = "Order items";
-          $collectionId = (new CollectionsServices)->getCollectionId($orderItemsCollectionName);
-          $url = $this->endpointUrl.'/'.$collectionId. '/items';
-          Http::pool(function(Pool $pool) use($url, $newOrderItems) {
-            foreach ($newOrderItems as $item) {
-              $pool->withHeaders($this->headers)->post($url, $item);
-            }
-          });
+    public function saveNewOrderItem($newOrderItems)
+    {
+      $orderItemsCollectionName = "Order items";
+      $collectionId = (new CollectionsServices)->getCollectionId($orderItemsCollectionName);
+      $url = $this->endpointUrl.'/'.$collectionId. '/items';
+      Http::pool(function(Pool $pool) use($url, $newOrderItems) {
+        foreach ($newOrderItems as $item) {
+          $pool->withHeaders($this->headers)->post($url, $item);
         }
+      });
+    }
   }
 
 ?>
